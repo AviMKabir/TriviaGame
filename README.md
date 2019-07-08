79666839a5fd41ae4c5975e6a4d97e3961a50ede
Avi M Kabir <avimishkakabir@gmail.com>
2019-07-08 13:52:53 -0400

    looping around nicely just gotta add the game mechanics of right and wrong aanswers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TriviaGame
 Timed Trivia Game
+
+live demo:  https://avimkabir.github.io/TriviaGame/
--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 
 
 
-var questions = ["question 1", "question 2", "question 3"];
+var questions = ["question 1", "question 2", "question 3", "question 4"];
 
 // holding the setInterval
 var showQuestion;
@@ -13,9 +13,50 @@ var count = 0;
 var correct = 0;
 var incorrect = 0;
 
+// TIMER STUFF 
+// timer number
+var number = 4;
+
+// holding timer setInterval
+var intervalId;
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  //  The decrement function.
+  function decrement() {
+
+    //  Decrease number by one.
+    number--;
+
+    //  Show the number in the #show-number tag.
+    $("#show-number").html("<h2>" + number + "</h2>");
+
+    if (number === 1) {
+
+number += 3
+
+    }
+
+  }
+
+  function stop() {
+
+    //  Clears our intervalId
+    //  We just pass the name of the interval
+    //  to the clearInterval function.
+    clearInterval(intervalId);
+  }
+
+
+
 // click to run game
 $("#start").click(startGame);
+
 $("#stop").click(stopGame);
+$("#stop").click(stop);
 
 
 
@@ -24,23 +65,32 @@ $("#stop").click(stopGame);
 // in the 'src' attribute of the img tag.
 function displayQuestion() {
   $("#questionHolder").html(questions[count]);
+
+  
 }
 
 function nextQuestion() {
+
+    run();
+
+    
   //  TODO: Increment the count by 1.
   count++;
 
   // TODO: Show the loading gif in the "image-holder" div.
   $("#questionHolder").html("this will be the loading screen");
+  $("#show-number").html("<h2>" + "0" + "</h2>");
+
 
   // TODO: Use a setTimeout to run displayImage after 1 second.
-  setTimeout(displayQuestion, 3000);
+  setTimeout(displayQuestion, 1000);
+
+  
 
   // TODO: If the count is the same as the length of the image array, reset the count to 0.
   if (count === questions.length) {
 
-    clearInterval(showQuestion);
-    $("#questionHolder").html("END");
+    count = 0;
 
     
 
@@ -48,9 +98,10 @@ function nextQuestion() {
 }
 
 function startGame() {
+    clearInterval(showQuestion);
 
   // TODO: Use showImage to hold the setInterval to run nextImage.
-  showQuestion = setInterval(nextQuestion, 9000);
+  showQuestion = setInterval(nextQuestion, 4000);
 
 }
 
@@ -62,7 +113,7 @@ function stopGame() {
 }
 
 // This will run the display image function as soon as the page loads.
-displayQuestion();
+
 
 
 
