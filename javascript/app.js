f2df13875de876ff015b9e9888596d7a938111be
Avi M Kabir <avimishkakabir@gmail.com>
2019-07-08 13:13:08 -0400

    added some script just a slideshow rn

--- javascript/app.js
+++ javascript/app.js
@@ -1,34 +1,74 @@
-var time = 30;
 
 
-var intervalId;
+
+var questions = ["question 1", "question 2", "question 3"];
+
+// holding the setInterval
+var showQuestion;
+
+// Count will keep track of the index
+var count = 0;
+
+// holding the amount of right & wrong answers
+var correct = 0;
+var incorrect = 0;
+
+// click to run game
+$("#start").click(startGame);
+$("#stop").click(stopGame);
 
 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
+
+
+// This function will replace display whatever image it's given
+// in the 'src' attribute of the img tag.
+function displayQuestion() {
+  $("#questionHolder").html(questions[count]);
 }
 
-function decrement() {
+function nextQuestion() {
+  //  TODO: Increment the count by 1.
+  count++;
 
-    time--;
+  // TODO: Show the loading gif in the "image-holder" div.
+  $("#questionHolder").html("this will be the loading screen");
 
-    $("#timerDiv").html(time);
+  // TODO: Use a setTimeout to run displayImage after 1 second.
+  setTimeout(displayQuestion, 3000);
 
-    if (time === 0) {
+  // TODO: If the count is the same as the length of the image array, reset the count to 0.
+  if (count === questions.length) {
 
-
-        stop();
-
-    }
-}
-
-function stop() {
+    clearInterval(showQuestion);
+    $("#questionHolder").html("END");
 
     
-    clearInterval(intervalId);
-  }
 
-run();
+  }
+}
+
+function startGame() {
+
+  // TODO: Use showImage to hold the setInterval to run nextImage.
+  showQuestion = setInterval(nextQuestion, 9000);
+
+}
+
+function stopGame() {
+
+  // TODO: Put our clearInterval here:
+  clearInterval(showQuestion);
+
+}
+
+// This will run the display image function as soon as the page loads.
+displayQuestion();
+
+
+
+
+
+
+
 
 
