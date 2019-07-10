f62b7bdbc6f5b635d6cba8dabb82f038685a0be5
Avi M Kabir <avimishkakabir@gmail.com>
2019-07-10 00:51:32 -0400

    game works perfectly just need to add some css

--- javascript/app.js
+++ javascript/app.js
@@ -2,15 +2,18 @@
 // i dont know anything about sonic but i needed a theme
 
 var questions = ["question 1: who is the blue hedgehog?",
-    "question 2: who is the cooler/darker version of him that uses guns?",
+    "question 2: who is the cooler/darker version of sonic that uses guns?",
     "question 3: who is the main bad guy in the sonic world?",
     "how many chaos emeralds are there in the original game?"];
+
 
 // holding the setInterval
 var showQuestion;
 
 // Count will keep track of the index
 var count = 0;
+
+
 
 // holding the amount of right & wrong answers
 var correct = 0;
@@ -19,7 +22,12 @@ var unanswered = 0;
 
 // state if the answer is correct or not
 
-var correctState = true
+var correctState = null;
+
+//gonna hold my stop variable because this decrement is not stopping
+
+var stopVariable = 0;
+
 
 // TIMER STUFF 
 // timer number
@@ -40,15 +48,15 @@ function decrement() {
     number--;
 
     //  Show the number in the #show-number tag.
-    $("#show-number").html("<h2>" + number + "</h2>");
+    $("#show-number").html("<h2> timer: " + number + "</h2>");
 
-    // did a little trick so between the questions i just add the seconds in and make it look like 0 if it's over 30 seconds lol
+    // did a little trick so between the questions i just add the seconds in and make it look like 0 if it's over 3 seconds lol
     // yeah I know i could trigger some other function to show a different timer but I really wanted to see if this would work
     // it does
 
     if (number > 3) {
 
-        $("#show-number").html("<h2>" + "0" + "</h2>");
+        $("#show-number").html("<h2> timer: " + "0" + "</h2>");
 
     }
 
@@ -58,6 +66,23 @@ function decrement() {
         number += 3
 
     }
+
+    if (count === questions.length) {
+
+
+
+        clearInterval(intervalId);
+        stop();
+        $("#show-number").html("<h2>timer: " + "" + "</h2>");
+
+
+
+
+
+
+    }
+
+
 
 }
 
@@ -69,22 +94,71 @@ function stop() {
     clearInterval(intervalId);
 }
 
+if (stopVariable = 1) {
 
+    stop();
+}
+
+
+function startGame() {
+
+    $( "#start" ).remove();
+
+    clearInterval(showQuestion);
+
+    // hold the setInterval to run nextquest
+    showQuestion = setInterval(nextQuestion, 4000);
+
+}
+
+function stopGame() {
+
+
+    stop();
+    // TODO: Put our clearInterval here:
+
+    clearInterval(showQuestion);
+    clearInterval(nextQuestion);
+    clearInterval(intervalId);
+    clearInterval(decrement);
+    clearInterval(run);
+
+
+}
+
+function restartGame() {
+
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+
+    // state if the answer is correct or not
+
+    correctState = null;
+
+    // TIMER STUFF 
+    // timer number
+    number = 4;
+    count = 0;
+
+    startGame();
+    displayQuestion();
+    $( "#restart" ).remove();
+
+
+
+
+
+}
 
 // click to run game
 $("#start").click(startGame);
 $("#start").click(displayQuestion);
-
-
-
-
-
-
-
-
-
-$("#stop").click(stopGame);
 $("#stop").click(stop);
+
+
+
+$("#restart").click(restartGame);
 
 
 
@@ -154,63 +228,63 @@ function nextQuestion() {
     // these are statements that log correct / incorrect answers
 
 
-    
+    var answerArr = ["sonic", "shadow", "eggman", "6"];
+    var answerVar = answerArr[count];
 
-        var radioValue = $("input[name='radiobtn']:checked").val();
+    var radioValue = $("input[name='radiobtn']:checked").val();
 
-        console.log(radioValue)
+    console.log(radioValue)
 
-        if (radioValue) {
+    if (radioValue) {
 
-            if (radioValue === "2") {
+        if (radioValue === "2") {
 
-                incorrect++;
-                correctState = false;
-
-
-            }
-
-            else if (radioValue === "1") {
-
-                correct++;
-                correctState = true;
-
-            }
-
+            incorrect++;
+            correctState = 0;
+            $("#questionHolder").html("incorrect! the answer was " + answerVar + "<br>" + "correct: " + correct + "<br>" + "incorrect: " + incorrect + "<br>" + "unanswered: " + unanswered);
 
 
         }
 
-        else {
+        else if (radioValue === "1") {
 
-            incorrect++
-            unanswered++
-            correctState = false;
+            correct++;
+            correctState = 1;
+            $("#questionHolder").html("correct! the answer was " + answerVar + "<br>" + "correct: " + correct + "<br>" + "incorrect: " + incorrect + "<br>" + "unanswered: " + unanswered);
+
 
         }
 
 
-    
+
+    }
+
+    else {
+
+        incorrect++
+        unanswered++
+        correctState = 0;
+        $("#questionHolder").html("incorrect! the answer was " + answerVar + "<br>" + "correct: " + correct + "<br>" + "incorrect: " + incorrect + "<br>" + "unanswered: " + unanswered);
+
+    }
+
+
+
 
 
     run();
 
-       // showing the results page
-       $("#questionHolder").html("correct: " + correct + "<br>" + "incorrect: " + incorrect + "<br>" + "unanswered: " + unanswered);
+
+
+
 
     //  Increment the count by 1.
     count++;
 
 
+    // just showing the number as 0 between
 
-
- 
-
-
-
-
-
-    $("#show-number").html("<h2>" + "0" + "</h2>");
+    $("#show-number").html("<h2> timer: " + "0" + "</h2>");
 
 
     // how long it will be up
@@ -218,10 +292,15 @@ function nextQuestion() {
 
 
 
-    // right now it ends the game but I can make it loop later
+    // right now it ends the game but I can make it loop later with my restart function
     if (count === questions.length) {
 
-        count = 0;
+        $("#questionHolder").html("FINAL SCORE" + "<br>" + "correct: " + correct + "<br>" + "incorrect: " + incorrect + "<br>" + "unanswered: " + unanswered);
+
+        stopGame();
+
+        $("#restartDiv").append( '<button id= "restart" class = "btn btn-success"> <i class="fa fa-circle-o" style="color:yellow;" aria-hidden="true"></i> RESTART <i class="fa fa-circle-o" style="color:yellow;" aria-hidden="true"></i></button> ' )
+        $("#restart").click(restartGame);
 
 
 
@@ -230,25 +309,7 @@ function nextQuestion() {
 
 
 
-function startGame() {
-    clearInterval(showQuestion);
 
-    // hold the setInterval to run nextquest
-    showQuestion = setInterval(nextQuestion, 4000);
-
-}
-
-function stopGame() {
-
-    // TODO: Put our clearInterval here:
-
-    clearInterval(showQuestion);
-
-    showQuestion = setInterval(nextQuestion, 4000);
-
-
-
-}
 
 
 
